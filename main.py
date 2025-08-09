afde924a137186c0ddd7c9985496a03615868620
Mahdee Ahmed <mahdeeahmed69@gmail.com>
2025-08-09 23:50:14 +0600

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello, Streamlit!")
-st.write("This is my first Streamlit app 🚀")
+st.title("Excel File Viewer 📊")
+
+# Upload Excel file
+uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls",])
+
+if uploaded_file is not None:
+    # Read file into DataFrame
+    df = pd.read_excel(uploaded_file)
+
+    st.subheader("Preview")
+    st.dataframe(df)  # Interactive table
+else:
+    st.info("Please upload an Excel file to view.")
 
