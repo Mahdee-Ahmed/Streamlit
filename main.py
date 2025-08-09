87f1d0f9e768c53e729d769e7af44bf45463004c
Mahdee Ahmed <mahdeeahmed69@gmail.com>
2025-08-09 23:55:00 +0600

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls",])
 
 if uploaded_file is not None:
     # Read file into DataFrame
-    df = pd.read_excel(uploaded_file)
+    df = pd.read_excel(uploaded_file,header=1)
 
     st.subheader("Preview")
     st.dataframe(df)  # Interactive table
